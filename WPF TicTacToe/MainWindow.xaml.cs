669ba6700583f932b22854083cb1b2493472fae3
Bo Loomans <bo.loomans204947@student.leeuwenborgh.nl>
2017-11-23 13:48:27 +0100

    PC

--- WPF TicTacToe/MainWindow.xaml.cs
+++ WPF TicTacToe/MainWindow.xaml.cs
@@ -20,9 +20,59 @@ namespace WPF_TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool PlayerTurn = true;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnTopLeft_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayerTurn == true)
+            {
+                ImgTopLeft.Source = new BitmapImage(new Uri("Images/Circle.jpg", UriKind.Relative));
+                btnTopLeft.IsEnabled = false;
+                PlayerTurn = false;
+            }
+            else
+            {
+                ImgTopLeft.Source = new BitmapImage(new Uri("Images/download.png", UriKind.Relative));
+                btnTopLeft.IsEnabled = false;
+                PlayerTurn = true;
+            }
+        }
+
+        private void btnMidLeft_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayerTurn == true)
+            {
+                ImgMidLeft.Source = new BitmapImage(new Uri("Images/Circle.jpg", UriKind.Relative));
+                btnMidLeft.IsEnabled = false;
+                PlayerTurn = false;
+            }
+            else
+            {
+                ImgMidLeft.Source = new BitmapImage(new Uri("Images/download.png", UriKind.Relative));
+                btnMidLeft.IsEnabled = false;
+                PlayerTurn = true;
+            }
+        }
+        
+        private void btnBotLeft_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayerTurn == true)
+            {
+                ImgBotLeft.Source = new BitmapImage(new Uri("Images/Circle.jpg", UriKind.Relative));
+                btnBotLeft.IsEnabled = false;
+                PlayerTurn = false;
+            }
+            else
+            {
+                ImgBotLeft.Source = new BitmapImage(new Uri("Images/download.png", UriKind.Relative));
+                btnBotLeft.IsEnabled = false;
+                PlayerTurn = true;
+            }
         }
     }
 }
