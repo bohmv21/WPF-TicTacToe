e8d590e92324d64812eecdd607fe029299aeb861
Bo Loomans <bo.loomans204947@student.leeuwenborgh.nl>
2017-11-24 18:40:31 +0100

    d

--- WPF TicTacToe/MainWindow.xaml.cs
+++ WPF TicTacToe/MainWindow.xaml.cs
@@ -30,206 +30,25 @@ namespace WPF_TicTacToe
             InitializeComponent();
         }
 
-        private void btnTopLeft_Click(object sender, RoutedEventArgs e)
+        private void Default_Click(object sender, RoutedEventArgs e)
         {
             Image button = (Image)((Button)sender).Content;
-            string Name = ((Button)sender).Name;
-            ArrayConverter(Name);
+            int Name = Convert.ToInt16(((Button)sender).Name.Remove(0,1));
 
             if (PlayerTurn == true)
             {
                 button.Source = new BitmapImage(new Uri("Images/Circle.png", UriKind.Relative));
-                ImgTopLeft.Visibility = Visibility.Visible;
-                btnTopLeft.IsEnabled = false;
-                WinCheck[0] = 1;
+                imgTurn.Source = new BitmapImage(new Uri("Images/Cross.png", UriKind.Relative));
+                WinCheck[Name] = 1;
             }
             else
             {
-                ImgTopLeft.Source = new BitmapImage(new Uri("Images/Cross.png", UriKind.Relative));
-                ImgTopLeft.Visibility = Visibility.Visible;
-                btnTopLeft.IsEnabled = false;
-                WinCheck[0] = 2;
+                button.Source = new BitmapImage(new Uri("Images/Cross.png", UriKind.Relative));
+                imgTurn.Source = new BitmapImage(new Uri("Images/Circle.png", UriKind.Relative));
+                WinCheck[Name] = 2;
             }
+            ((Button)sender).IsEnabled = false;
             PlayerTurn = !PlayerTurn;
-            WinChecker();
-        }
-
-        private void btnMidLeft_Click(object sender, RoutedEventArgs e)
-        {
-            if (PlayerTurn == true)
-            {
-                ImgMidLeft.Source = new BitmapImage(new Uri("Images/Circle.png", UriKind.Relative));
-                ImgMidLeft.Visibility = Visibility.Visible;
-                btnMidLeft.IsEnabled = false;
-                PlayerTurn = false;
-                WinCheck[1] = 1;
-            }
-            else
-            {
-                ImgMidLeft.Source = new BitmapImage(new Uri("Images/Cross.png", UriKind.Relative));
-                ImgMidLeft.Visibility = Visibility.Visible;
-                btnMidLeft.IsEnabled = false;
-                PlayerTurn = true;
-                WinCheck[1] = 2;
-            }
-
-            WinChecker();
-        }
-
-        private void btnBotLeft_Click(object sender, RoutedEventArgs e)
-        {
-            if (PlayerTurn == true)
-            {
-                ImgBotLeft.Source = new BitmapImage(new Uri("Images/Circle.png", UriKind.Relative));
-                ImgBotLeft.Visibility = Visibility.Visible;
-                btnBotLeft.IsEnabled = false;
-                PlayerTurn = false;
-                WinCheck[2] = 1;
-            }
-            else
-            {
-                ImgBotLeft.Source = new BitmapImage(new Uri("Images/Cross.png", UriKind.Relative));
-                ImgBotLeft.Visibility = Visibility.Visible;
-                btnBotLeft.IsEnabled = false;
-                PlayerTurn = true;
-                WinCheck[2] = 2;
-            }
-
-            WinChecker();
-        }
-
-        private void btnMidTop_Click(object sender, RoutedEventArgs e)
-        {
-            if (PlayerTurn == true)
-            {
-                ImgMidTop.Source = new BitmapImage(new Uri("Images/Circle.png", UriKind.Relative));
-                ImgMidTop.Visibility = Visibility.Visible;
-                btnMidTop.IsEnabled = false;
-                PlayerTurn = false;
-                WinCheck[3] = 1;
-            }
-            else
-            {
-                ImgMidTop.Source = new BitmapImage(new Uri("Images/Cross.png", UriKind.Relative));
-                ImgMidTop.Visibility = Visibility.Visible;
-                btnMidTop.IsEnabled = false;
-                PlayerTurn = true;
-                WinCheck[3] = 2;
-
-            }
-
-            WinChecker();
-        }
-
-        private void btnMid_Click(object sender, RoutedEventArgs e)
-        {
-            if (PlayerTurn == true)
-            {
-                ImgMid.Source = new BitmapImage(new Uri("Images/Circle.png", UriKind.Relative));
-                ImgMid.Visibility = Visibility.Visible;
-                btnMid.IsEnabled = false;
-                PlayerTurn = false;
-                WinCheck[4] = 1;
-
-            }
-            else
-            {
-                ImgMid.Source = new BitmapImage(new Uri("Images/Cross.png", UriKind.Relative));
-                ImgMid.Visibility = Visibility.Visible;
-                btnMid.IsEnabled = false;
-                PlayerTurn = true;
-                WinCheck[4] = 2;
-            }
-
-            WinChecker();
-        }
-
-        private void btnMidBot_Click(object sender, RoutedEventArgs e)
-        {
-            if (PlayerTurn == true)
-            {
-                ImgMidBot.Source = new BitmapImage(new Uri("Images/Circle.png", UriKind.Relative));
-                ImgMidBot.Visibility = Visibility.Visible;
-                btnMidBot.IsEnabled = false;
-                PlayerTurn = false;
-                WinCheck[5] = 1;
-
-            }
-            else
-            {
-                ImgMidBot.Source = new BitmapImage(new Uri("Images/Cross.png", UriKind.Relative));
-                ImgMidBot.Visibility = Visibility.Visible;
-                btnMidBot.IsEnabled = false;
-                PlayerTurn = true;
-                WinCheck[5] = 2;
-            }
-
-            WinChecker();
-        }
-
-        private void btnRightTop_Click(object sender, RoutedEventArgs e)
-        {
-            if (PlayerTurn == true)
-            {
-                ImgRightTop.Source = new BitmapImage(new Uri("Images/Circle.png", UriKind.Relative));
-                ImgRightTop.Visibility = Visibility.Visible;
-                btnRightTop.IsEnabled = false;
-                PlayerTurn = false;
-                WinCheck[6] = 1;
-            }
-            else
-            {
-                ImgRightTop.Source = new BitmapImage(new Uri("Images/Cross.png", UriKind.Relative));
-                ImgRightTop.Visibility = Visibility.Visible;
-                btnRightTop.IsEnabled = false;
-                PlayerTurn = true;
-                WinCheck[6] = 2;
-            }
-
-            WinChecker();
-        }
-
-        private void btnRightMid_Click(object sender, RoutedEventArgs e)
-        {
-            if (PlayerTurn == true)
-            {
-                ImgRightMid.Source = new BitmapImage(new Uri("Images/Circle.png", UriKind.Relative));
-                ImgRightMid.Visibility = Visibility.Visible;
-                btnRightMid.IsEnabled = false;
-                PlayerTurn = false;
-                WinCheck[7] = 1;
-            }
-            else
-            {
-                ImgRightMid.Source = new BitmapImage(new Uri("Images/Cross.png", UriKind.Relative));
-                ImgRightMid.Visibility = Visibility.Visible;
-                btnRightMid.IsEnabled = false;
-                PlayerTurn = true;
-                WinCheck[7] = 2;
-            }
-
-            WinChecker();
-        }
-
-        private void btnRightBot_Click(object sender, RoutedEventArgs e)
-        {
-            if (PlayerTurn == true)
-            {
-                ImgRightBot.Source = new BitmapImage(new Uri("Images/Circle.png", UriKind.Relative));
-                ImgRightBot.Visibility = Visibility.Visible;
-                btnRightBot.IsEnabled = false;
-                PlayerTurn = false;
-                WinCheck[8] = 1;
-            }
-            else
-            {
-                ImgRightBot.Source = new BitmapImage(new Uri("Images/Cross.png", UriKind.Relative));
-                ImgRightBot.Visibility = Visibility.Visible;
-                btnRightBot.IsEnabled = false;
-                PlayerTurn = true;
-                WinCheck[8] = 2;
-            }
-
             WinChecker();
         }
 
@@ -240,15 +59,6 @@ namespace WPF_TicTacToe
                 WinCheck[0] == 1 && WinCheck[3] == 1 && WinCheck[6] == 1 || WinCheck[1] == 1 && WinCheck[4] == 1 && WinCheck[7] == 1 || WinCheck[2] == 1 && WinCheck[5] == 1 && WinCheck[8] == 1)
             {
                 txtWin.Text = "Player 1 Wins";
-                btnTopLeft.IsEnabled = false;
-                btnMidLeft.IsEnabled = false;
-                btnBotLeft.IsEnabled = false;
-                btnMidTop.IsEnabled = false;
-                btnMid.IsEnabled = false;
-                btnMidBot.IsEnabled = false;
-                btnRightTop.IsEnabled = false;
-                btnRightMid.IsEnabled = false;
-                btnRightBot.IsEnabled = false;
                 txtPopup.Text = "P1 Wins!!";
                 popup.IsOpen = true;
             }
@@ -257,15 +67,6 @@ namespace WPF_TicTacToe
                 WinCheck[0] == 2 && WinCheck[3] == 2 && WinCheck[6] == 2 || WinCheck[1] == 2 && WinCheck[4] == 2 && WinCheck[7] == 2 || WinCheck[2] == 2 && WinCheck[5] == 2 && WinCheck[8] == 2)
                  {
                 txtWin.Text = "Player 2 Wins";
-                btnTopLeft.IsEnabled = false;
-                btnMidLeft.IsEnabled = false;
-                btnBotLeft.IsEnabled = false;
-                btnMidTop.IsEnabled = false;
-                btnMid.IsEnabled = false;
-                btnMidBot.IsEnabled = false;
-                btnRightTop.IsEnabled = false;
-                btnRightMid.IsEnabled = false;
-                btnRightBot.IsEnabled = false;
                 txtPopup.Text = "P2 Wins!!";
                 popup.IsOpen = true;
 
@@ -274,42 +75,34 @@ namespace WPF_TicTacToe
             {
                 txtWin.Text = "Tie!";
             }
-
         }
 
-        private void ArrayConverter(string sender)
+        private void Reset_Click(object sender, RoutedEventArgs e)
         {
-            switch (sender)
+            Image[] arrSource = {ImgTopLeft , ImgMidLeft , ImgBotLeft , ImgMidTop, ImgMid, ImgMidBot , ImgRightTop, ImgRightMid, ImgRightBot , imgTurn};
+
+            _0.IsEnabled = true;
+            _1.IsEnabled = true;
+            _2.IsEnabled = true;
+            _3.IsEnabled = true;
+            _4.IsEnabled = true;
+            _5.IsEnabled = true;
+            _6.IsEnabled = true;
+            _7.IsEnabled = true;
+            _8.IsEnabled = true;
+
+            foreach (Image element in arrSource)
             {
-                case "btnTopLeft":
-
-                    break;
+                element.Source = null;
             }
-        }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            btnTopLeft.IsEnabled = true;
-            btnMidLeft.IsEnabled = true;
-            btnBotLeft.IsEnabled = true;
-            btnMidTop.IsEnabled = true;
-            btnMid.IsEnabled = true;
-            btnMidBot.IsEnabled = true;
-            btnRightTop.IsEnabled = true;
-            btnRightMid.IsEnabled = true;
-            btnRightBot.IsEnabled = true;
+            foreach (int element in WinCheck)
+            {
+                int i = 0;
+                WinCheck[i] = 0;
+                i++;
+            }
 
-            ImgTopLeft.Visibility = Visibility.Hidden;
-            ImgMidLeft.Visibility = Visibility.Hidden;
-            ImgBotLeft.Visibility = Visibility.Hidden;
-            ImgMidTop.Visibility = Visibility.Hidden;
-            ImgMid.Visibility = Visibility.Hidden;
-            ImgMidBot.Visibility = Visibility.Hidden;
-            ImgRightTop.Visibility = Visibility.Hidden;
-            ImgRightMid.Visibility = Visibility.Hidden;
-            ImgRightBot.Visibility = Visibility.Hidden;
-
-            WinCheck[0] = 0;
             WinCheck[1] = 0;
             WinCheck[2] = 0;
             WinCheck[3] = 0;
@@ -325,7 +118,7 @@ namespace WPF_TicTacToe
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            Button_Click(sender, e);
+            Reset_Click(sender, e);
             popup.IsOpen = false;
         }
     }
